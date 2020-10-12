a0dfdcaef9ef2fe07ea3b60627203f8fa60c49be
Chelsey Bergmann <chelseybergmann@email.arizona.edu>
2020-10-12 13:04:55 -0700

    Basic UI View and navigations through app.

---
+++ RoadXpert/RoadXpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadXpertApp.swift
+//  RoadXpert
+//
+//  Created by Chelsey Bergmann on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct RoadXpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
