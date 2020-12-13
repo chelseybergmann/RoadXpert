75af00b0cef7e3d36ec84057474b61adde88692c
Chelsey Bergmann <chelseybergmann@email.arizona.edu>
2020-12-13 00:06:57 -0600

    Initial Commit

---
+++ RoadXpert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoadXpert
+//
+//  Created by Chelsey Bergmann on 12/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
